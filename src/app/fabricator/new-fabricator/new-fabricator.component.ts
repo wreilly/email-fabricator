eb5b7f8c961b78479259f64b99d4e6470eb1fa0f
William Reilly <wreilly2001@gmail.com>
2020-02-02 15:15:02 -0500

    NewFabricator fabricating e-mails; navigate to (still empty) FabricatorResults

--- src/app/fabricator/new-fabricator/new-fabricator.component.ts
+++ src/app/fabricator/new-fabricator/new-fabricator.component.ts
@@ -1,5 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormGroupDirective  } from '@angular/forms';
+/* Hmm, do we need NgForm? (for error matcher)
+Seems not so far. FormGroupDirective for Reactive Forms, NgForm for Template-Driven?
+ */
+
+import { Router } from '@angular/router';
+
+import {ErrorStateMatcher} from '@angular/material'; // MatInput property
+/*
+Example above where v. specific Material Design best imported
+just here in Component where used,
+rather than in app-wide MyMaterialModule.
+
+- "new() up in ngOnInit()
+- Then used here by this.myResetForm()
+(See .HTML matInput fields too).
+ */
+
+class MyErrorStateMatcherClass implements ErrorStateMatcher {
+  isErrorState(controlPassedIn: FormControl | null, form: FormGroupDirective | null): boolean {
+    // Experiment: left off NgForm above
+    return !!(
+        controlPassedIn
+        &&
+        controlPassedIn.invalid
+        &&
+        (
+            controlPassedIn.dirty
+            ||
+            controlPassedIn.touched
+        )
+    );
+/* We OMIT to include form.submitted (which is the default, and which is causing us the trouble trying to RESET the form after submission.
+ */
+  }
+}
 
 @Component({
   selector: 'app-new-fabricator',
@@ -15,6 +50,9 @@ export class NewFabricatorComponent implements OnInit {
   I am using "Group" for my own purposes in the app, while Angular Reactive Forms of course uses Group for their "FormGroup". Sorry!
    */
   myFabricatorFormControlGroup: FormControl; // << It's a Control, not a "Group"
+
+  myOwnErrorStateMatcher: MyErrorStateMatcherClass;
+  // My own implementation. Cheers.
 
 
 /* BETTER Prob. Just "TYPE" them:
@@ -36,7 +74,9 @@ export class NewFabricatorComponent implements OnInit {
   myListOfStrings: string;
   myStackOfStrings: string;
 
-  constructor() { }
+  constructor(
+      private myRouter: Router,
+  ) { }
 
   ngOnInit(): void {
 
@@ -78,10 +118,11 @@ Object
     this.myFabricatorFormGroup = new FormGroup({
       myFabricatorFormControlStudentIncrementerCounterName: this.myFabricatorFormControlStudentIncrementerCounter,
       myFabricatorFormControlNumberOfRowsName  : this.myFabricatorFormControlNumberOfRows,
-
       myFabricatorFormControlGroupName: this.myFabricatorFormControlGroup
 
     });
+
+    this.myOwnErrorStateMatcher = new MyErrorStateMatcherClass();
 
   } // /ngOnInit()
 
@@ -115,13 +156,74 @@ Object
 
     this.fabricateEmailAddresses();
 
+    // Maybe can put first? but still, Prob not needed ... since navigate away hey?
+    this.myResetForm();
+
+    this.myRouter.navigate(['fabricator/results-fabricator'])
+        .then(() => {
+          // empty Promise
+          // btw, yeah, you do need the encompassing folder there above 'fabricator/' Not entirely sure how come. cheers.
+        });
+
+    // Prob not needed ... since navigate away hey?
+    // this.myResetForm();
+
   }
 
   fabricateEmailAddresses() {
+
     console.log('inside fabricate()! ', this.myStudentIncrementerCounter); // yep e.g. 1
     console.log('inside fabricate()! ', this.myNumberOfRows); // yep e.g. 100
     console.log('inside fabricate()! ', this.myGroup); // yep e.g. 'studentgroupname'
 
+    // let myArray = []; // << OK initialized as (empty) []
+    // let myArray; // << OK just declared
+    // let myArray: []; // << NOT OK - ts-lint complained when I'd "typed" it. hmmph.
+/* This two-part thing was complain-y.
+
+    let myArray = Array(this.myNumberOfRows - this.myStudentIncrementerCounter);
+
+    myArray.fill(null).map(() => `student-${this.myGroup}-${this.myStudentIncrementerCounter++}@hbsp.harvard.edu`);
+*/
+
+/* No. Wrong logic. (worked for simplest, first case: 1 to 25 and such...
+
+    let myArrayOfAddresses = Array(this.myNumberOfRows - ( this.myStudentIncrementerCounter - 1 ) )
+*/
+    let myArrayOfAddresses = Array(this.myNumberOfRows ) // << Array length is simply number of rows !!!
+        .fill(null).map(() => `student-${this.myGroup}-${this.myStudentIncrementerCounter++}@hbsp.harvard.edu`);
+    // N.B. MINUS ONE!
+
+    // Get all Strings out of Array, into a single comma-separated string
+    let myListOfStringsOfAddresses = myArrayOfAddresses.toString();
+
+    // Turn comma-separated LIST of Strings into carriage-return-separated STACK (as 'twere) of Strings, via RegEx:
+    let myStackOfStringsOfAddresses = myListOfStringsOfAddresses.replace(/(student-[0-9]+-[0-9]+@hbsp.harvard.edu),/g, '$1\n');
+
+    console.log('HERE IT IS! (whoa) \n =================\n');
+    console.log(myStackOfStringsOfAddresses);
+    console.log('\n =================\n');
+
+    /* Hmm...
+    Should we reset those consts herein?
+    Hmm.
+     */
+    myArrayOfAddresses = [];
+    myListOfStringsOfAddresses = '';
+    myStackOfStringsOfAddresses = '';
+  }
+
+  myResetForm() {
+    this.myFabricatorFormGroup.reset();
+    /*
+    Curious bug/issue with resetting - does not "reset" VALIDATION. (sigh)
+    Happily, Max tackled this for us in Udemy Angular Material Design course (thank god/God). See LoginComponent
+    Needs whole custom "ErrorStateMatcher"; see also HTML input matInput
+
+    DOCK-O (quel idée!?)
+https://material.angular.io/components/input/overview#changing-when-error-messages-are-shown
+
+     */
   }
 
 }
@@ -132,7 +234,9 @@ HEW-2461.txt
 JAVASCRIPT to create e-mail addresses to paste into Excel
 ======================
 // TODO:
-myStudentIncrementerCounter = 0
+let myStudentIncrementerCounter = 0
+let myCounter = 0
+
 let myNumberOfRows = 25
 
 let myGroup = '01'
@@ -147,7 +251,9 @@ let myStackOfStrings = ''
 
 // Populate Array with string concatenated, counter-incremented, invented user e-mail addresses.
 
-myArray = Array(myNumberOfRows - myCounter).fill().map(() => `student-${myGroup}-${myCounter++}@hbsp.harvard.edu`)
+myArray = Array(myNumberOfRows - myCounter).fill(null).map(() => `student-${myGroup}-${myCounter++}@hbsp.harvard.edu`)
+// About that NULL (in .fill())
+https://medium.com/@wisecobbler/4-ways-to-populate-an-array-in-javascript-836952aea79f
 
 myArray
 // tslint:disable-next-line:max-line-length
