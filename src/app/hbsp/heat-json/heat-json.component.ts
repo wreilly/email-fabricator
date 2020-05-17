f1f28ebb6afc9c5f0727d2b02a43b1867b34471a
William Reilly <wreilly2001@gmail.com>
2020-05-17 09:12:09 -0400

    import * as JSONfile .default vs FileReader string .result

--- src/app/hbsp/heat-json/heat-json.component.ts
+++ src/app/hbsp/heat-json/heat-json.component.ts
@@ -31,16 +31,38 @@ HECS-6195  We have large JSON file of 115 Pending Educator Profiles
 // 1. THREE HARD-CODED FILES I CAN USE (Comment Out two)
 // import * as data from './myjson-20200508-112.json';
 // import * as data from './myjson-20200510-59.json';
-import * as data from './myjson-20200512-65.json';
+/*
+import * as dataFromHardCodedFile from './myjson-20200512-65.json';
+*/
+import * as dataFromHardCodedFile from '../../../../../email-fabricator-assets-out-of-git/myjson-20200512-65.json';
+/* Go UP and OUT of Git to store this file:
+$ pwd
+/Users/william.reilly/dev/Angular/projects
+$ ls
+email-fabricator // << Git
+email-fabricator-assets-out-of-git // << Out of Git
+$ ls email-fabricator-assets-out-of-git/
+myjson-20200512-65.json
+
+Alternative of course: Keep within project folder, but put into .gitignore
+*/
+/* N.B. import as JSON actually DOES get you JSON. (quel idée)
+No need to JSON.parse() for HARD-CODED FILE import * as data (it's ALREADY JSON!) << YEP! :)
+*/
 import {Observable, Subscription} from 'rxjs';
 // 2. "dataFromChooser" will be from FileReader, below
 
 // A. Ok
 let dataFromChooser: string | ArrayBuffer; // FileReader.result type
-// C. Ok
-let myHeatAuthorizations: any; // Q. What is this? A. [{},{}]
-// [{"username":"arkangel.cordero" ...}]
 
+// C. Ok
+/*
+let myHeatAuthorizations: any; // Q. What is this? A. [{},{}]
+*/
+// [{"username":"arkangel.cordero" ...}]
+// Pint-sized finding: Q. Why is this declaration way up here, outside class?
+// A. No good reason (MBU). Let's move it INSIDE class. Then, can be SEEN in template
+//       where we use it for [disabled] logic on button. cheers.
 
 @Component({
   selector: 'app-heat-json',
@@ -48,6 +70,21 @@ let myHeatAuthorizations: any; // Q. What is this? A. [{},{}]
   styleUrls: ['./heat-json.component.css']
 })
 export class HeatJsonComponent implements OnInit, AfterViewInit {
+
+  // SEE NGAFTERVIEWINIT()
+  /* Notes from class: fitness-tracker-wr3
+https://www.udemy.com/angular-full-app-with-angular-material-angularfire-ngrx/learn/lecture/9120380#questions/7276854
+The "static" option is an Angular 8 thing. Wasn't in 7, and in Angular 9 won't be needed...
+Also:
+BTW: If the element (MatSort, MatPaginator) would be used in ngOnInit(), you would have to write { static: true }.
+(We use it in ngAfterViewInit(), so false is okay)
+https://angular.io/guide/static-query-migration
+(*) https://angular.io/guide/static-query-migration#what-does-this-flag-mean-and-why-is-it-necessary
+*/
+
+  constructor(
+      private myHbspService: HbspService,
+  ) { }
 
   // FILE CHOOSER / SELECTOR
   // https://stackblitz.com/edit/angular-material-file-select?file=src%2Fapp%2Fapp.component.ts
@@ -67,25 +104,11 @@ export class HeatJsonComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator)myPaginator: MatPaginator; // {static: false } nor true either. didn't help. leaving out. cheers?
 
   @ViewChild(MatSort, { static: false }) mySort: MatSort;
-
-  // SEE NGAFTERVIEWINIT()
-  /* Notes from class: fitness-tracker-wr3
-https://www.udemy.com/angular-full-app-with-angular-material-angularfire-ngrx/learn/lecture/9120380#questions/7276854
-The "static" option is an Angular 8 thing. Wasn't in 7, and in Angular 9 won't be needed...
-Also:
-BTW: If the element (MatSort, MatPaginator) would be used in ngOnInit(), you would have to write { static: true }.
-(We use it in ngAfterViewInit(), so false is okay)
-https://angular.io/guide/static-query-migration
-(*) https://angular.io/guide/static-query-migration#what-does-this-flag-mean-and-why-is-it-necessary
-*/
-
-  constructor(
-      private myHbspService: HbspService,
-  ) { }
+  myHeatAuthorizations: any; // WAS declared above outside class.
 
   ngOnInit(): void {
 
-    console.log('JSON Data! ? ', data);
+    console.log('JSON dataFromHardCodedFile! ? ', dataFromHardCodedFile);
 
   }
 
@@ -126,6 +149,9 @@ webkitRelativePath: ""
      */
 
     const myFileReaderToReadMyFile = new FileReader();
+
+    // This next block DEFINES the File Reader's ".onload()" method
+    // Just below is the call to INVOKE this onload runner...
     myFileReaderToReadMyFile.onload = (myEvent: any) => {
       console.log('myEvent ', myEvent);
       /* ProgressEvent
@@ -136,19 +162,48 @@ webkitRelativePath: ""
       readyState: 2
        */
 
-      dataFromChooser = myFileReaderToReadMyFile.result;
+      /* CHOOSER vs. HARD-CODED - getting at the array of users data
+      Chooser needs ".result" :)
+      Hard-Coded needs ".default" :)
+       */
+      dataFromChooser = myFileReaderToReadMyFile.result; // << .result needed! :)
       console.log('dataFromChooser ', dataFromChooser);
       /*
       {"errors":[],"data":{"users":[{"username":"arkangel.cordero"...
        */
 
-      myHeatAuthorizations = (dataFromChooser as any);
-      console.log('myOnChangeFileInpu() ONLOAD Result: myHeatAuthorizations ', myHeatAuthorizations);
+      this.myHeatAuthorizations = (dataFromChooser as any);
+      // tslint:disable-next-line:max-line-length
+      console.log('myOnChangeFileInput() ONLOAD Result 01 dataFromChooser: this.myHeatAuthorizations ', this.myHeatAuthorizations);
       /* (Same Object)
       {"errors":[],"data":{"users":[{"username":"arkangel.cordero",
        */
-    };
+
+      // ***** TESTING **********
+      // ** DONE. PASSED. Veddy nice. **
+      // We'll temporarily *HIJACK* here:  (DONE)
+      // this.myHeatAuthorizations = (dataFromHardCodedFile as any); // << Nope!
+      // tslint:disable-next-line:max-line-length
+      // this.myHeatAuthorizations = (dataFromHardCodedFile as any).default.data.users; // << .default needed!  PLUS ".data.users". okay << NOPE!
+/* WORKED:
+      this.myHeatAuthorizations = (dataFromHardCodedFile as any).default; // << .default needed! :) No - don't do .data.users here << YEP
+*/
+      // 1. YES WORKS:
+      /*
+      const myHeatAuthorizations: any = (data as any).default;
+      */
+      /* Note: ".default" is name of OBJECT { } holding all content in the .json file, as seen apparently from a JSON "import" OK.
+      */
+
+/* No longer needed (was for hijack testing)
+      console.log('myOnChangeFileInput() ONLOAD Result 02 dataFromHardCodedFile: this.myHeatAuthorizations ', this.myHeatAuthorizations);
+*/
+
+    }; // /.onload()
+
     myFileReaderToReadMyFile.readAsText(this.myFile);
+    // This line RUNS the File Reader's  .onload() method, just above
+    // That is, I guess .readAsText() is one method (among a few) that in essence invokes .onload(). cheers
   }
 
   myParseOutEmails() {
@@ -156,7 +211,7 @@ webkitRelativePath: ""
 
     // console.log('myParseOutEmails() myHeatAuthorizations.data ', myHeatAuthorizations.data); // undefined
     // console.log('myParseOutEmails() myHeatAuthorizations.data.users ', myHeatAuthorizations.data.users);
-    console.log('myParseOutEmails() myHeatAuthorizations ', myHeatAuthorizations);
+    console.log('myParseOutEmails() this.myHeatAuthorizations ', this.myHeatAuthorizations);
     /* wtf? looks to be right ?
     {
 "errors": [],
@@ -166,7 +221,16 @@ webkitRelativePath: ""
 Q. Hmm, I think I need to JSON.parse() this string into JSON. Hmm
 A. OH YEAH BABY-KIN OH YEAH!
      */
-    const myHeatAuthorizationsAsJson = JSON.parse(myHeatAuthorizations);
+
+/* YES do JSON.parse() for FILE CHOOSER .result (it's a STRING!)
+*/
+    const myHeatAuthorizationsAsJson = JSON.parse(this.myHeatAuthorizations);
+
+/* BUT do NOT do JSON.parse() for HARD-CODED FILE import * as data (it's ALREADY JSON!) (I think/hope/whatever) << YEP! :)
+
+    const myHeatAuthorizationsAsJson = this.myHeatAuthorizations; // whamma
+*/
+
     console.log('myParseOutEmails() myHeatAuthorizationsAsJson ', myHeatAuthorizationsAsJson);
     // core.js:5873 ERROR TypeError: Cannot read property 'users' of undefined at NewFabricatorComponent.myParseOutEmails
     this.educators = myHeatAuthorizationsAsJson.data.users
