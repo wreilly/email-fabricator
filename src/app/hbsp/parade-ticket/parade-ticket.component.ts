54142acc61a356be6092dd462161a987d22719e0
William Reilly <wreilly2001@gmail.com>
2021-09-07 08:36:13 -0400

    Parade Ticket Component; Hardcoded "BE" call direct to JIRA, get data

---
+++ src/app/hbsp/parade-ticket/parade-ticket.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parade-ticket',
+  templateUrl: './parade-ticket.component.html',
+  styleUrls: ['./parade-ticket.component.css']
+})
+export class ParadeTicketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
