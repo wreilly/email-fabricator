73be5f12f1c8bc432f53780cc25e12bfa4cfa9e4
William Reilly <wreilly2001@gmail.com>
2021-09-13 05:48:42 -0400

    Config tweaks; /dist, going to PROD Env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
   'nytimes-api-key-top-stories': 'STMA4A1DcdjKxY68rwVzYFAlC2EficSF',
+  jiraProxyServer: {
+    apiUrl: 'http://0.0.0.0',
+    apiPort: '3000',
+    apiVersion: 'api/v1',
+  }
 };
