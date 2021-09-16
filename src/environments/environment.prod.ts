0c8553bc514bdb555c43d89f4a7f70ab9b465a44
William Reilly <wreilly2001@gmail.com>
2021-09-16 08:06:30 -0400

    Client. env file needed AWS URL ec2-100...etc.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   'nytimes-api-key-top-stories': 'STMA4A1DcdjKxY68rwVzYFAlC2EficSF',
   jiraProxyServer: {
-    apiUrl: 'http://0.0.0.0',
+    apiUrl: 'http://ec2-100-26-170-247.compute-1.amazonaws.com',
     apiPort: '3000',
     apiVersion: 'api/v1',
   }
